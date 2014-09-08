c20a677e679a997a64b3820f2af1726dc813fd3b
Spegelius <spegelius@gmail.com>
2014-09-08 18:02:34 +0300

    Copy also selective-spn-confs

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -31,6 +31,10 @@ PRODUCT_COPY_FILES := \
     vendor/aosp/etc/apns-conf.xml:system/etc/apns-conf.xml \
     vendor/aosp/etc/spn-conf.xml:system/etc/spn-conf.xml 
 
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/aosp/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 # Enable ADB authentication
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
